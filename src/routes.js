c3a5a8f354e76901d89465db7bdaf9c8869cbe81
‘Katrin <katrin.baier77@gmail.com>
2022-11-04 14:56:24 +0100

    update controller

--- src/routes.js
+++ src/routes.js
@@ -3,10 +3,6 @@ import express from 'express';
 
 const pokeRouter = express.Router();
 
-// pokeRouter.route('/').get(getAllPokemons);
-// pokeRouter.route('/:id').get(getOnePokemon);
-// pokeRouter.route('/:id/:info').get(getOnePokemonsCategory);
-
 pokeRouter.get('/', getAllPokemons);
 pokeRouter.get('/:id', getOnePokemon);
 pokeRouter.get('/:id/:info', getOnePokemonsCategory);
